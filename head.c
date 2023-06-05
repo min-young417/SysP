4ebbcd2d60f3128ec3dc09829dceb4c9fa3e12c6
minyoung kim <nonamed1021@naver.com>
2023-06-05 14:19:52 +0900

    update head

--- head.c
+++ head.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     int opt;
     int verbose = 0;
     int quiet = 0;
+    int num_minus = 0;
 
     int num_lines = 10;
     const char *file_name;
     FILE *file;
-    char line[256];
-    int line_count = 0;
 
-    	
     while ((opt = getopt(argc, argv, "vqn:")) != -1) {
         switch (opt) {
             case 'v':
@@ -23,7 +22,11 @@ int main(int argc, char *argv[]) {
 		quiet = 1;
 		break;
 	    case 'n':
-		num_lines = atoi(optarg);
+		if (optarg[0] == '-') {
+		    num_minus = 1;
+		    num_lines = -1 * atoi(optarg);
+		} else
+		    num_lines = atoi(optarg);
 		break;
             default:
                 fprintf(stderr, "사용법: %s [옵션] [파일]\n", argv[0]);
@@ -49,12 +52,27 @@ int main(int argc, char *argv[]) {
             printf("==> %s <==\n", file_name);
         }
 
-        line_count = 0;
-        while (fgets(line, sizeof(line), file) != NULL && line_count < num_lines) {
-            printf("%s", line);
-            line_count++;
-        }
+	char buffer[100];
+        char lines[100][100];
+        int lineCount = 0;
+	int endLine = 0;
 
+        while (fgets(buffer, sizeof(buffer), file) != NULL) {
+            strcpy(lines[lineCount], buffer);
+            lineCount++;
+        }
+	
+	if(num_lines < lineCount)
+	    endLine = num_lines;
+	else
+	    endLine = lineCount;
+	if(num_minus)
+	    endLine = lineCount - num_lines; 
+
+        for (int j = 0; j < endLine; j++) {
+            printf("%s", lines[j]);
+        }
+	
 	if (!(i == argc-1) && !quiet){
             printf("\n");
         }
