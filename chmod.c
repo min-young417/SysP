ecdd23acfc7862181e08a88bc1def28dbe851bd5
minyoung kim <nonamed1021@naver.com>
2023-06-09 18:05:38 +0900

    update chmod

--- chmod.c
+++ chmod.c
@@ -14,11 +14,8 @@ int main(int argc, char *argv[]) {
     int permission = strtol(permissionString, NULL, 8);
 
     if (chmod(filePath, permission) == -1) {
-        printf("퍼미션 변경에 실패했습니다.\n");
         return 1;
     }
-
-    printf("퍼미션이 성공적으로 변경되었습니다.\n");
 
     return 0;
 }
